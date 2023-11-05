2800ecd5b57171ca2d7e5cbba6502935ad21e2c3
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-05 19:23:42 +0300

    Reformatted promises file using prettier extension.

--- promises.js
+++ promises.js
@@ -1,37 +1,29 @@
-function getWeather(){
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            resolve("Sunny");
-        }, 1000);
-         
-    })
+function getWeather() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve("Sunny");
+    }, 1000);
+  });
 }
 
-function greetings(data){
-    return new Promise((resolve,reject)=>{
-        setTimeout(() => {
-            
-            if (data=="Sunny") {
-                resolve("GOOD MORNING");
-                
-            } else  {
-                reject("Error Not Found!!");
-            }
-        }, 1000);
-        
-    })
-    
+function greetings(data) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (data == "Sunny") {
+        resolve("GOOD MORNING");
+      } else {
+        reject("Error Not Found!!");
+      }
+    }, 1000);
+  });
 }
 
-function onSuccess(data){
-    console.log(`Success: ${data}`);
+function onSuccess(data) {
+  console.log(`Success: ${data}`);
 }
 
-function onFailure(error){
-    console.log(`Failure: ${error}`);
+function onFailure(error) {
+  console.log(`Failure: ${error}`);
 }
 
-getWeather()
-    .then(greetings)
-        .then(onSuccess)
-            .catch(onFailure);
+getWeather().then(greetings).then(onSuccess).catch(onFailure);
