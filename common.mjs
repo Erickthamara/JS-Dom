92f1c3d759b8cf4e346318527eb8762ea4ce5536
Erickthamara <105923301+Erickthamara@users.noreply.github.com>
2023-11-05 18:42:58 +0300

    Realized my Prettier extension wasn't working so i reformatted the two files.

--- common.mjs
+++ common.mjs
@@ -1,19 +1,17 @@
-export const api_Token_url="https://accounts.spotify.com/api/token";
-export const client_id='';
-export const secret_key='';
+export const api_Token_url = "https://accounts.spotify.com/api/token";
+export const client_id = "";
+export const secret_key = "";
 
-export async function getApiToken(){
-    const data= await fetch(api_Token_url,{
-        method:'POST',
-        headers:{
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization': 'Basic ' + btoa(client_id + ':' + secret_key)
-        },
-        body: 'grant_type=client_credentials'
-
-    });
-    const info= await data.json();
-    //console.log(info.access_token);
-    return info.access_token;
-    
+export async function getApiToken() {
+  const data = await fetch(api_Token_url, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: "Basic " + btoa(client_id + ":" + secret_key),
+    },
+    body: "grant_type=client_credentials",
+  });
+  const info = await data.json();
+  //console.log(info.access_token);
+  return info.access_token;
 }
--- spotify_API.mjs
+++ spotify_API.mjs
@@ -1,25 +1,25 @@
-import { getApiToken } from './common.mjs';
+import { getApiToken } from "./common.mjs";
 
 //Sauti Sol unique address
 //https://open.spotify.com/artist/4Rj9lQm9oSiMlirgpsM6eo?si=MXreUfzMRRKQZGsrUSgd1g
 
-
 //Diamond Platnumz
 //https://open.spotify.com/artist/3cAisWS37sGCCtRgWfvrod?si=arSxkGERR02toPhBWjqZwA
-const token= await getApiToken()
+const token = await getApiToken();
 
-const artistData=async()=>{
-    
-    const artist_data=await fetch("https://api.spotify.com/v1/artists/3cAisWS37sGCCtRgWfvrod?si=arSxkGERR02toPhBWjqZwA",{
-        method : 'GET',
-        headers : {
-            'Authorization':  'Bearer ' + token
-        }
-        }
-    );
-    const info=await artist_data.json()
-    console.log(info.name);
-    console.log(info.genres);
-}
+const artistData = async () => {
+  const artist_data = await fetch(
+    "https://api.spotify.com/v1/artists/3cAisWS37sGCCtRgWfvrod?si=arSxkGERR02toPhBWjqZwA",
+    {
+      method: "GET",
+      headers: {
+        Authorization: "Bearer " + token,
+      },
+    }
+  );
+  const info = await artist_data.json();
+  console.log(info.name);
+  console.log(info.genres);
+};
 
-artistData()
+artistData();
